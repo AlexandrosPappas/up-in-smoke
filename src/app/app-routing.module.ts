3e7ce24dd345ab136399f1eaffcdc5244eb4bdac
AlexandrosPappas <pappas.g.alexandros@gmail.com>
2021-07-27 12:52:07 +0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DrinkListComponent } from './drink-list/drink-list.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { OffersComponent } from './offers/offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'home'
+},
+{
+  path: 'home', component:HomeComponent
+},
+{
+  path: 'menu', component:DrinkListComponent
+},
+{
+  path: 'gallery', component:GalleryComponent
+},
+{
+  path: 'offers', component:OffersComponent
+},
+{
+  path: '**',
+  redirectTo:'home'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DrinkListComponent } from './drink-list/drink-list.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { OffersComponent } from './offers/offers.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DrinkListComponent,
+    GalleryComponent,
+    NavigationComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
