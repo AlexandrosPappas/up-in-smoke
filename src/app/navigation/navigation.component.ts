c8708af7d687e12617c2bfd55cb41df00b03dfff
Alexandros Pappas <pappas.g.alexandros@gmail.com>
2021-07-29 23:06:13 +0300

    mobile-nav-try-2

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Output, ViewChild } from '@angular/core';
+import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
-
+export class NavigationComponent implements OnInit, AfterViewInit {
+@ViewChild('v')  native: any;
+isMobile = false 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(){
+    debugger
+    console.log(this.native.nativeElement)
+  //  if(this.native?.className = 'burger'){
+  //    this.isMobile = true;
+
+  //  }else {
+  //    this.isMobile = false
+  //  }
+
+  }
+o(){
+  alert('yo')
+}
 }
