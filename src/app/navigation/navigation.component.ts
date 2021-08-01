fa8d65de85d7b13bb9cbb865f6d82be1b19e02fe
AlexandrosPappas <pappas.g.alexandros@gmail.com>
2021-08-01 15:00:28 +0300

    mobile-navigation-button

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component, OnInit,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, AfterViewInit {
+  @ViewChild('mobileNav', {static:true}) mobileNav:any
+  isMobile = false;
+  constructor(){}
 
-  constructor() { }
+  ngOnInit(): void{}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    console.log(this.mobileNav)
   }
-
+  collapse(){
+    if(!this.isMobile) {
+      this.isMobile = true
+    }else{
+      this.isMobile = false;
+    }
+  }
 }
