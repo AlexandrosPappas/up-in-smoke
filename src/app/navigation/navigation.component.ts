204de626db1336c164712adea47a241d0ceab92a
AlexandrosPappas <pappas.g.alexandros@gmail.com>
2021-08-21 21:10:51 +0300

    developing-offers-2-re-upload

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import {
   AfterViewInit,
   Component, OnInit,
   ViewChild
 } from '@angular/core';
-=======
-import { AfterViewInit, Component, OnInit, Output, ViewChild } from '@angular/core';
-import { EventEmitter } from 'stream';
->>>>>>> c8708af7d687e12617c2bfd55cb41df00b03dfff
 
 @Component({
   selector: 'app-navigation',
@@ -15,22 +10,15 @@ import { EventEmitter } from 'stream';
   styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit, AfterViewInit {
-<<<<<<< HEAD
   @ViewChild('mobileNav', {static:true}) mobileNav:any
   isMobile = false;
   constructor(){}
 
   ngOnInit(): void{}
-=======
-@ViewChild('v')  native: any;
-isMobile = false 
-  constructor() { }
->>>>>>> c8708af7d687e12617c2bfd55cb41df00b03dfff
 
   ngAfterViewInit() {
     console.log(this.mobileNav)
   }
-<<<<<<< HEAD
   collapse(){
     if(!this.isMobile) {
       this.isMobile = true
@@ -39,21 +27,3 @@ isMobile = false
     }
   }
 }
-=======
-
-  ngAfterViewInit(){
-    debugger
-    console.log(this.native.nativeElement)
-  //  if(this.native?.className = 'burger'){
-  //    this.isMobile = true;
-
-  //  }else {
-  //    this.isMobile = false
-  //  }
-
-  }
-o(){
-  alert('yo')
-}
-}
->>>>>>> c8708af7d687e12617c2bfd55cb41df00b03dfff
